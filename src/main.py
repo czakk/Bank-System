e4db4ba918c0916b3d09bf3d9b3a3a4e2b6489e7
czak <banaczykczarek@gmail.com>
2021-02-23 21:34:06 +0100

    adding User Class

--- src/main.py
+++ src/main.py
@@ -7,14 +7,15 @@ load_dotenv()
 def display_hello():
     """Function display hello message for user"""
     print('''Hello in our Bank System. We hope you enjoy our service!''')
-    print('''Log-in to your account. If you still don\'t have account in our Bank, please $ign-in
-    1. Log-in
-    2. Sign-in''')
-    choice = input("Choice, please.: ")
-    if choice == "1":
-        login()
-    if choice == "2":
-        signin()
+    while True:
+        print('''Log-in to your account. If you still don\'t have account in our Bank, please $ign-in
+        1. Log-in
+        2. Sign-in''')
+        choice = input("Choice, please.: ")
+        if choice == "1":
+            return login()
+        if choice == "2":
+            signin()
 
 
 def login() -> tuple:
@@ -23,11 +24,11 @@ def login() -> tuple:
         login = input("Please enter your login: ")
         passwd = input("and password: ")
 
-        user = db.get_user('users', login, passwd)
+        user = db.get_user('users', login.lower().strip(), passwd)
         if user is False:
             print("No user in database")
         else:
-            print(f"Hi {user[1]}")
+            print(f"Hi {user[1].title()}")
             return user
 
 
@@ -43,16 +44,25 @@ def signin():
                 if i.isupper(): isupper = True
                 if i.isalnum(): isint = True
         if db.check_login_taken('users', login) is True:
-            db.add_user('users', login.lower(), passwd)
+            db.add_user('users', login.lower().strip(), passwd)
             print("Register Complete")
             break
         else:
             print("Login is Taken! Try Again.")
 
 
+class User:
+    def __init__(self, user_info: tuple):
+        self.userid = user_info[0]
+        self.name = user_info[1]
+        self.amount = user_info[3]
+
+
 def main():
-    display_hello()
+    user_information = display_hello()
+    user = User(user_information)
 
 
 if __name__ == '__main__':
     main()
+
--- src/main_test.py
+++ src/main_test.py
@@ -3,8 +3,15 @@ from main import *
 
 
 def test_login():
-    result = (5, 'test', 'test', 0.0)
+    result: tuple[int, str, str, float] = (5, 'test', 'test', 0.0)
     with mock.patch("builtins.input", return_value="test"):
         assert login() == result
 
 
+class TestUser:
+    def test_on_create(self):
+        user = User((0, 'czak', 'test', 100))
+        assert user.userid == 0
+        assert user.name == 'czak'
+        assert user.amount == 100
+        assert isinstance(user, User)
